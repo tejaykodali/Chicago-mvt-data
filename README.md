bfc15f00db1d00b85e3b0904c12fe8dc51550a42
Teja K <yaswateja@gmail.com>
2015-08-18 14:31:58 +0530

    new files

--- README.md
+++
@@ -1,8 +0,0 @@
-## README
-Here, the motor vehicle theft data from Chicago, taken from the <a href = "http://gis.chicagopolice.org/">Chicago
-Police Department</a> is analysed. Three plots are generated:
-  1. Hourly number of thefts committed each day
-  2. Heatmap showing when most of the crimes occur
-  3. Heatmap showing where in Chicago crimes occur
-  
-If you have any questions, feel free to reach out to me via email, or <a href = "https://twitter.com/tejaykodali">Twitter</a>.
---
+++ chicagoMVT.r
@@ -0,0 +1,58 @@
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+library(maps)
+library(ggmap)
+
+## Reading in the data
+chicagoMVT <- read.csv('motor_vehicle_theft.csv', stringsAsFactors = FALSE)
+
+## Converting the date to a recognizable format
+chicagoMVT$Date <- strptime(chicagoMVT$Date, format = '%m/%d/%Y %I:%M:%S %p')
+
+## Getting the day and hour of each crime
+chicagoMVT$Day <- weekdays(chicagoMVT$Date)
+chicagoMVT$Hour <- chicagoMVT$Date$hour
+
+## Sorting the weekdays
+dailyCrimes <- as.data.frame(table(chicagoMVT$Day, chicagoMVT$Hour))
+names(dailyCrimes) <- c('Day', 'Hour', 'Freq')
+dailyCrimes$Hour <- as.numeric(as.character(dailyCrimes$Hour))
+dailyCrimes$Day <- factor(dailyCrimes$Day, ordered = TRUE, 
+                         levels = c('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'))
+
+## Plotting the number of crimes each day (line graph)
+png(filename = 'dailyCrimes.png', width = 800, height = 600, units = 'px')
+ggplot(dailyCrimes, aes(x = Hour, y = Freq)) + geom_line(aes(group = Day, color = Day)) + 
+  xlab('Hour') + ylab('Number of thefts') + ggtitle('Daily number of Motor Vehicle Thefts')
+dev.off()
+
+## Plotting the number of crimes each day (heat map)
+png(filename = 'heatmap.png', width = 800, height = 600, units = 'px')
+ggplot(dailyCrimes, aes(x = Hour, y = Day)) + geom_tile(aes(fill = Freq)) + 
+  scale_fill_gradient(name = 'Total Motor Vehicle Thefts', low = 'black', high = 'red') + 
+  theme(axis.title.y = element_blank(), plot.background = element_rect(fill = 'gray13', color = 'white'),
+        panel.background = element_rect(fill = 'black', color = 'white'))
+dev.off()
+
+## Removing empty locations and splitting Location into Latitude and Longitude
+chicagoMVT$Location[chicagoMVT$Location == ''] <- NA
+chicagoMVT <- na.omit(chicagoMVT)
+chicagoMVT <- chicagoMVT %>% extract(Location, c('Latitude', 'Longitude'), '\\(([^,]+), ([^)]+)\\)')
+chicagoMVT$Longitude <- round(as.numeric(chicagoMVT$Longitude), 2)
+chicagoMVT$Latitude <- round(as.numeric(chicagoMVT$Latitude), 2)
+
+## Get Chicago map and crime locations
+chicago <- get_map(location = 'chicago', zoom = 11)
+locationCrimes <- as.data.frame(table(chicagoMVT$Longitude, chicagoMVT$Latitude))
+names(locationCrimes) <- c('long', 'lat', 'Frequency')
+locationCrimes$long <- as.numeric(as.character(locationCrimes$long))
+locationCrimes$lat <- as.numeric(as.character(locationCrimes$lat))
+locationCrimes <- subset(locationCrimes, Freq > 0)
+
+## Plotting the location heatmap
+png(filename = "Chicagomap.png", width = 800, height = 600, units = "px")
+ggmap(chicago) + geom_tile(data = locationCrimes, aes(x = long, y = lat, alpha = Frequency),
+                           fill = "red") + theme(axis.title.y = element_blank(), axis.title.x = element_blank())
+dev.off()
+
